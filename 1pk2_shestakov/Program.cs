dcfbdf357c39cea9ce0afcad4246df8d4415d36f
a <a@DESKTOP-HF74V2R>
2025-01-26 17:06:46 +0500

    Добавьте файлы проекта.

---
+++ 1pk2_shestakov/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _1pk2_shestakov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
